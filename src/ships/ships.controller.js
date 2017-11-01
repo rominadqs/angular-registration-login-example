29cbaa05bcab3daca1c010ca361acb2153a0a655
rominadqs <querecuto.romina@gmail.com>
2017-11-01 13:53:14 +0100

    [task 2] custom list

--- src/ships/ships.controller.js
+++ src/ships/ships.controller.js
@@ -3,21 +3,35 @@
 
     angular
         .module('app')
-        .controller('ShipsController', ShipsController);
+        .controller('ShipsController', ShipsController)
+        .component('customMdList', {
+            templateUrl: 'ships/ship.component.view.html',
+            controllerAs: "vm",
+            controller: "ShipsController",
+        });
 
-    ShipsController.$inject = ['ShipsService'];
     function ShipsController(ShipsService) {
         var vm = this;
 
+        vm.list = [];
+        vm.limit = 5;
+        vm.more = more;
         initController();
 
         function initController() {
             ShipsService.Starship()
             .then(function (result) {
                 console.log(result.data);
+                vm.list = result.data.results;
             });
         }
 
+        function more(){
+            vm.limit += vm.limit;
+        }
+
     }
+
+    ShipsController.$inject = ['ShipsService'];
 
 })();
